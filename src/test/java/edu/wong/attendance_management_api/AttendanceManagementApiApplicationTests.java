82a43f53419abf38332caa018edd9cbf215a3325
WongSilver <783244553@qq.com>
2022-03-20 01:16:15 +0800

    找回config文件

---
+++ src/test/java/edu/wong/attendance_management_api/AttendanceManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wong.attendance_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
