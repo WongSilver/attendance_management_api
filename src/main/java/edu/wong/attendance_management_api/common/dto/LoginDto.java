038fa4512ade6f0bedd64b182f7c5145d2c637fc
WongSilver <783244553@qq.com>
2022-03-21 20:17:23 +0800

    增加用户DTO

---
+++ src/main/java/edu/wong/attendance_management_api/common/dto/LoginDto.java
@@ -0,0 +1,20 @@
+package edu.wong.attendance_management_api.common.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Data
+public class LoginDto implements Serializable {
+
+    //    用户名
+    @NotBlank(message = "用户名不能为空")
+    private String name;
+
+
+    //    用户密码
+    @NotBlank(message = "密码不能为空")
+    private String password;
+
+}
