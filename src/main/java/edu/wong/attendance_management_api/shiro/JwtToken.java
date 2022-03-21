c43f1d51efbb5bf6cdbcf328b1fbc09e6f487d58
WongSilver <783244553@qq.com>
2022-03-21 20:20:47 +0800

    添加注释

--- src/main/java/edu/wong/attendance_management_api/shiro/JwtToken.java
+++ src/main/java/edu/wong/attendance_management_api/shiro/JwtToken.java
@@ -2,9 +2,12 @@ package edu.wong.attendance_management_api.shiro;
 
 import org.apache.shiro.authc.AuthenticationToken;
 
+/**
+ *  shiro默认supports的是UsernamePasswordToken，而我们现在采用了jwt的方式，所以这里我们自定义一个JwtToken，来完成shiro的supports方法。
+ */
 public class JwtToken implements AuthenticationToken {
 
-    private String token;
+    private final String token;
 
     public JwtToken(String token) {
         this.token = token;
