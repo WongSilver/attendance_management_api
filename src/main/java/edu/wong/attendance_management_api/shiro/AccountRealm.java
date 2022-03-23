71025888a99a58e51ecac1885e67234d55b05db0
WongSilver <783244553@qq.com>
2022-03-23 15:13:39 +0800

    修复查询 权限和角色的bug

--- src/main/java/edu/wong/attendance_management_api/shiro/AccountRealm.java
+++ src/main/java/edu/wong/attendance_management_api/shiro/AccountRealm.java
@@ -1,13 +1,15 @@
 package edu.wong.attendance_management_api.shiro;
 
 import edu.wong.attendance_management_api.common.lang.ResponseFormat;
-import edu.wong.attendance_management_api.entity.*;
+import edu.wong.attendance_management_api.entity.Right;
+import edu.wong.attendance_management_api.entity.User;
+import edu.wong.attendance_management_api.entity.UserRole;
 import edu.wong.attendance_management_api.mapper.RightMapper;
 import edu.wong.attendance_management_api.mapper.RoleMapper;
-import edu.wong.attendance_management_api.mapper.RoleRightMapper;
 import edu.wong.attendance_management_api.mapper.UserRoleMapper;
 import edu.wong.attendance_management_api.service.IUserService;
 import edu.wong.attendance_management_api.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
@@ -16,9 +18,11 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+@Slf4j
 @Component
 public class AccountRealm extends AuthorizingRealm {
 
@@ -29,12 +33,9 @@ public class AccountRealm extends AuthorizingRealm {
     @Resource
     private UserRoleMapper userRoleMapper;
     @Resource
-    private RoleRightMapper roleRightMapper;
-    @Resource
     private RoleMapper roleMapper;
     @Resource
     private RightMapper rightMapper;
-    private List<Role> roles;
     private List<Right> rights;
 
     //    根据token判断此Authenticator是否使用该realm
@@ -46,6 +47,10 @@ public class AccountRealm extends AuthorizingRealm {
     //    授权
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        this.clearCachedAuthenticationInfo(principalCollection);
+        this.clearCachedAuthorizationInfo(principalCollection);
 //        获取token对应的用户
         User user = (User) principalCollection.getPrimaryPrincipal();
 
@@ -55,38 +60,36 @@ public class AccountRealm extends AuthorizingRealm {
             userMap.put("user_id", user.getId());
             List<UserRole> userRoles = userRoleMapper.selectByMap(userMap);
 
+//            System.out.println("==================>角色<==================");
 //            通过角色ID 查询 角色名
             HashMap<String, Object> roleMap = new HashMap<>();
-            userRoles.forEach(role -> roleMap.put(role.getRoleId().toString(), role.getRoleId()));
-            roles = roleMapper.selectByMap(roleMap);
+            ArrayList<Integer> rightList = new ArrayList<>();
+            userRoles.forEach(userRole -> {
+//            可能会出现一个用户多个角色，查询的权限名放入info
+//            查询的角色ID放入List给查询权限使用
+                roleMap.put("id", userRole.getRoleId());
+                roleMapper.selectByMap(roleMap).forEach(role -> {
+                    info.addRole(role.getName());
+                    rightList.add(role.getId());
+//                    System.out.println("==================>" + role.getName());
+                });
+            });
 
+//            System.out.println("==================>权限<==================");
+//            遍历rightList，把该用户所有的角色ID取出来
+//            通过 角色ID 查询 可操作的Url
+//            添加权限到Shiro
+            rightList.forEach(id -> {
+                rights = rightMapper.selectRightUrlByRoleID(id);
+                rights.forEach(url -> {
+                    info.addStringPermission(url.getUrl());
+//                    System.out.println("==================>" + url.getUrl());
+                });
+            });
 
-//            通过 角色ID 查询 权限ID
-            HashMap<String, Object> rightMap = new HashMap<>();
-            userRoles.forEach(o -> rightMap.put(o.getRoleId().toString(), o.getRoleId()));
-            List<RoleRight> roleRights = roleRightMapper.selectByMap(rightMap);
-
-//            通过 权限ID 查询 可操作url
-            HashMap<String, Object> urlMap = new HashMap<>();
-            roleRights.forEach((o -> urlMap.put(o.getRightId().toString(), o.getRightId())));
-            rights = rightMapper.selectByMap(urlMap);
-
-            rights.forEach(o -> System.out.println(o.getUrl()));
         } catch (Exception e) {
             ResponseFormat.fail("权限查询失败");
         }
-
-        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-//        添加角色
-        roles.forEach(role -> info.addRole(role.getName()));
-//        添加权限
-        rights.forEach(url -> info.addRole(url.getUrl()));
-
-//        test
-        System.out.println("--------角色---------");
-        roles.forEach(System.out::println);
-        System.out.println("--------权限---------");
-        rights.forEach(System.out::println);
 
         return info;
     }
@@ -94,6 +97,7 @@ public class AccountRealm extends AuthorizingRealm {
     //    认证
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+//        System.out.println("-------------认证--------------");
         JwtToken jwtToken = (JwtToken) token;
         String subject = util.getClaimByToken((String) jwtToken.getPrincipal()).getSubject();
 
@@ -107,6 +111,5 @@ public class AccountRealm extends AuthorizingRealm {
 //        否则返回SimpleAuthenticationInfo
 //        包装SimpleAuthenticationInfo第一个参数，用户一些非重要的信息
         return new SimpleAuthenticationInfo(user, jwtToken.getCredentials(), getName());
-//        return new SimpleAuthenticationInfo();
     }
 }
