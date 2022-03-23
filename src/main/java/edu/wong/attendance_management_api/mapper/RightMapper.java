6053a957fcd4736a8cc21db939fe1beac01d8696
WongSilver <783244553@qq.com>
2022-03-23 15:19:55 +0800

    写一些复杂的查询逻辑

--- src/main/java/edu/wong/attendance_management_api/mapper/RightMapper.java
+++ src/main/java/edu/wong/attendance_management_api/mapper/RightMapper.java
@@ -1,16 +1,20 @@
 package edu.wong.attendance_management_api.mapper;
 
-import edu.wong.attendance_management_api.entity.Right;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.wong.attendance_management_api.entity.Right;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author WongSilver
  * @since 2022-03-20
  */
 public interface RightMapper extends BaseMapper<Right> {
-
+    //    通过角色ID 查询 权限Url
+    List<Right> selectRightUrlByRoleID(@Param("id") int id);
 }
