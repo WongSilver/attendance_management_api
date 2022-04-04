df63ea149c039ae58024bdb68c99b5d16e43fef9
WongSilver <783244553@qq.com>
2022-04-04 22:51:07 +0800

    捕获ExpiredCredentialsException

--- src/main/java/edu/wong/attendance_management_api/common/exception/GlobalException.java
+++ src/main/java/edu/wong/attendance_management_api/common/exception/GlobalException.java
@@ -3,6 +3,7 @@ package edu.wong.attendance_management_api.common.exception;
 import edu.wong.attendance_management_api.common.lang.ResponseFormat;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.ShiroException;
+import org.apache.shiro.authc.ExpiredCredentialsException;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
@@ -20,6 +21,13 @@ public class GlobalException {
     @ExceptionHandler(value = ShiroException.class)
     public ResponseFormat handler(ShiroException e) {
         log.error("未授权异常", e);
+        return ResponseFormat.operate(401, e.getMessage(), null);
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(value = ExpiredCredentialsException.class)
+    public ResponseFormat handler(ExpiredCredentialsException e) {
+        log.error("token已失效，请重新登录 ", e);
         return ResponseFormat.operate(401, e.getMessage(), null);
     }
 
@@ -43,7 +51,7 @@ public class GlobalException {
     @ExceptionHandler(value = UnauthorizedException.class)
     public ResponseFormat handler(UnauthorizedException e) {
         log.error("权限异常", e);
-        return ResponseFormat.fail("权限异常:" + e.getMessage());
+        return ResponseFormat.operate(401, e.getMessage(), null);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
