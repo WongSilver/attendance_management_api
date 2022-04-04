e76a949610f0a6f3f006d6608911361fb9cc75ee
WongSilver <783244553@qq.com>
2022-04-04 22:52:39 +0800

    添加swagger配置

---
+++ src/main/java/edu/wong/attendance_management_api/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package edu.wong.attendance_management_api.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+    /**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     */
+    @Bean
+    public Docket restApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("标准接口")
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(true)
+                .forCodeGeneration(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("edu.wong.attendance_management_api.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://127.0.0.1:8088/swagger-ui/index.html
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger APIs")
+                .contact(new Contact("WongSilver", "https://github.com/WongSilver", "783244553@qq.com"))
+                .version("1.0")
+                .build();
+    }
+}
