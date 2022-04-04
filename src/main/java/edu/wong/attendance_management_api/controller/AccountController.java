37fa4418d2d0513445b8c902345061ae7812c9d8
WongSilver <783244553@qq.com>
2022-04-04 22:50:14 +0800

    新增部分用户功能

--- src/main/java/edu/wong/attendance_management_api/controller/AccountController.java
+++ src/main/java/edu/wong/attendance_management_api/controller/AccountController.java
@@ -2,19 +2,17 @@ package edu.wong.attendance_management_api.controller;
 
 import cn.hutool.core.map.MapUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import edu.wong.attendance_management_api.common.dto.LoginDto;
+import edu.wong.attendance_management_api.entity.dto.LoginDto;
 import edu.wong.attendance_management_api.common.lang.ResponseFormat;
 import edu.wong.attendance_management_api.entity.User;
+import edu.wong.attendance_management_api.mapper.AccountMapper;
 import edu.wong.attendance_management_api.service.IUserService;
 import edu.wong.attendance_management_api.shiro.JwtToken;
 import edu.wong.attendance_management_api.util.JwtUtil;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
@@ -29,8 +27,11 @@ public class AccountController {
     JwtUtil jwtUtil;
     private String jwt;
 
+    @Resource
+    AccountMapper mapper;
+
     @PostMapping("/login")
-    public ResponseFormat login(@Validated @RequestBody LoginDto dto, HttpServletResponse response) {
+    public ResponseFormat login(@Validated @RequestBody(required = false) LoginDto dto, HttpServletResponse response) {
 //        查询数据库是否存在该用户
         User user = service.getOne(new QueryWrapper<User>().eq("name", dto.getName()));
         if (user == null) {
@@ -39,8 +40,8 @@ public class AccountController {
         if (!user.getPassword().equals(dto.getPassword())) {
             return ResponseFormat.fail("密码不正确");
         }
-//       用户名密码正确 token不存在 生成Token返回给前端
-        if (jwt == null) {
+//       用户名密码正确 token不存在或者失效 生成Token返回给前端
+        if (jwt == null || jwtUtil.isTokenExpired(jwtUtil.getClaimByToken(jwt).getExpiration())) {
             jwt = jwtUtil.generateToken(user);
         }
         response.setHeader("Authorization", jwt);
@@ -53,6 +54,7 @@ public class AccountController {
                 .put("id", user.getId())
                 .put("groupId", user.getGroupId())
                 .put("mail", user.getMail())
+                .put("token", jwt)
                 .map());
     }
 
@@ -61,6 +63,12 @@ public class AccountController {
     public ResponseFormat logout() {
         SecurityUtils.getSubject().logout();
         return ResponseFormat.successful(null);
+    }
+
+    @GetMapping("/info/{id}")
+    public ResponseFormat Info(@PathVariable int id) {
+        System.out.println("---------------------");
+        return ResponseFormat.successful(mapper.selectInfoByUserId(id));
     }
 
 }
