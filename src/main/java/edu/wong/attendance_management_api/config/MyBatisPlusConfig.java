360261897e538d65fcbee4c86fbfaa4d1a81c8d9
WongSilver <783244553@qq.com>
2022-04-04 22:54:22 +0800

    使用新的分页拦截器方法

--- src/main/java/edu/wong/attendance_management_api/config/MyBatisPlusConfig.java
+++ src/main/java/edu/wong/attendance_management_api/config/MyBatisPlusConfig.java
@@ -1,6 +1,7 @@
 package edu.wong.attendance_management_api.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -15,7 +16,9 @@ public class MyBatisPlusConfig {
 
     //    MyBatisPlus 分页拦截器
     @Bean
-    public PaginationInnerInterceptor paginationInnerInterceptor() {
-        return new PaginationInnerInterceptor(DbType.MYSQL);
+    public MybatisPlusInterceptor paginationInnerInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 }
