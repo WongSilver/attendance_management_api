b6c122525a25407c565273d0d5bac426aa7d9f10
WongSilver <783244553@qq.com>
2022-04-12 09:28:37 +0800

    新增全局异常捕获

--- src/main/java/edu/wong/attendance_management_api/common/exception/GlobalException.java
+++ src/main/java/edu/wong/attendance_management_api/common/exception/GlobalException.java
@@ -4,34 +4,29 @@ import edu.wong.attendance_management_api.common.lang.ResponseFormat;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.ShiroException;
 import org.apache.shiro.authc.ExpiredCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authz.UnauthorizedException;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalException {
-
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = ShiroException.class)
     public ResponseFormat handler(ShiroException e) {
         log.error("未授权异常", e);
         return ResponseFormat.operate(401, e.getMessage(), null);
     }
 
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = ExpiredCredentialsException.class)
     public ResponseFormat handler(ExpiredCredentialsException e) {
         log.error("token已失效，请重新登录 ", e);
-        return ResponseFormat.operate(401, e.getMessage(), null);
+        return ResponseFormat.operate(999, e.getMessage(), null);
     }
 
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseFormat handler(MethodArgumentNotValidException e) {
         log.error("实体校验异常", e);
@@ -40,28 +35,30 @@ public class GlobalException {
         return ResponseFormat.operate(401, objectError.getDefaultMessage(), null);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseFormat handler(IllegalArgumentException e) {
         log.error("Assert时异常", e);
         return ResponseFormat.fail(e.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = UnauthorizedException.class)
     public ResponseFormat handler(UnauthorizedException e) {
         log.error("权限异常", e);
-        return ResponseFormat.operate(401, e.getMessage(), null);
+        return ResponseFormat.operate(233, e.getMessage(), null);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = LockedAccountException.class)
+    public ResponseFormat handler(LockedAccountException e) {
+        log.error("用户被锁定，请联系管理员", e);
+        return ResponseFormat.fail(e.getMessage());
+    }
+
     @ExceptionHandler(value = RuntimeException.class)
     public ResponseFormat handler(RuntimeException e) {
         log.error("运行时异常", e);
         return ResponseFormat.fail(e.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = Exception.class)
     public ResponseFormat handler(Exception e) {
         log.error("异常", e);
