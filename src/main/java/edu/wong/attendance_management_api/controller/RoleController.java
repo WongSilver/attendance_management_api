65bf88d11568fdf4f61ad16e542cf0722e4c32b7
WongSilver <783244553@qq.com>
2022-04-14 08:28:07 +0800

    新增：list、add、all、setRole

--- src/main/java/edu/wong/attendance_management_api/controller/RoleController.java
+++ src/main/java/edu/wong/attendance_management_api/controller/RoleController.java
@@ -1,18 +1,128 @@
 package edu.wong.attendance_management_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import edu.wong.attendance_management_api.common.lang.ResponseFormat;
+import edu.wong.attendance_management_api.entity.Role;
+import edu.wong.attendance_management_api.entity.UserRole;
+import edu.wong.attendance_management_api.mapper.RoleMapper;
+import edu.wong.attendance_management_api.mapper.UserRoleMapper;
+import edu.wong.attendance_management_api.service.IRoleService;
+import edu.wong.attendance_management_api.service.IUserRoleService;
+import org.apache.logging.log4j.util.Strings;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author WongSilver
  * @since 2022-03-20
  */
-@Controller
+@RestController
 @RequestMapping("/role")
 public class RoleController {
+
+    @Resource
+    private IRoleService service;
+    @Resource
+    private RoleMapper mapper;
+    @Resource
+    private IUserRoleService userRoleService;
+    @Resource
+    private UserRoleMapper userRoleMapper;
+
+    @GetMapping("/all")
+    public ResponseFormat findAll() {
+        return ResponseFormat.successful(service.list());
+    }
+
+    //    查询用户列表
+    @RequiresRoles("admin")
+    @GetMapping("/list")
+    public ResponseFormat list(@RequestParam(defaultValue = "1") Integer currentPage, @RequestParam(defaultValue = "10") Integer pageSize, @RequestParam(defaultValue = "") String roleName, @RequestParam(defaultValue = "") String roleType, @RequestParam(defaultValue = "") String roleStatus) {
+
+        IPage<Role> page = new Page<>(currentPage, pageSize);   // myBatisPlus自带的分页方法,参数一：起始页，参数二：多少条数据
+        LambdaQueryWrapper<Role> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+
+        lambdaQueryWrapper.like(Strings.isNotEmpty(roleName), Role::getName, roleName);
+        lambdaQueryWrapper.like(Strings.isNotEmpty(roleType), Role::getType, roleType);
+        lambdaQueryWrapper.like(Strings.isNotEmpty(roleStatus), Role::getStatus, roleStatus);
+
+        IPage<Role> pageData = service.page(page, lambdaQueryWrapper);
+
+        return ResponseFormat.successful(pageData);
+    }
+
+
+    @PostMapping("/edit")
+    public ResponseFormat edit(@RequestBody Role role) {
+        boolean b;
+        QueryWrapper<Role> name = new QueryWrapper<Role>().eq("name", role.getName());
+        List<Role> roles = mapper.selectList(name);
+
+//        前端传来的ID为空提示未登录
+//        前端传来的ID查询后存在 提示权限名存在的
+        if (role.getId() == null) {
+            return ResponseFormat.fail("未登录");
+        } else if (!roles.isEmpty()) {
+            return ResponseFormat.fail("用户已存在");
+        } else {
+            b = service.updateById(role);
+        }
+        return b ? ResponseFormat.successful(null) : ResponseFormat.fail("失败");
+    }
+
+
+    /**
+     * 添加或修改用户信息
+     *
+     * @param
+     * @return ResponseFormat
+     */
+    @PostMapping("/add")
+    public ResponseFormat add(@RequestBody Role role) {
+        /**
+         * 逻辑：同用户
+         */
+        if (role.getId() != null) {
+            Role temp = mapper.selectOne(new QueryWrapper<Role>().eq("name", role.getName()));
+            if (temp == null || role.getId().equals(temp.getId())) {
+                return ResponseFormat.successful(service.saveOrUpdate(role));
+            }
+            return ResponseFormat.fail("用户名已存在");
+        }
+        if (mapper.selectCount(new QueryWrapper<Role>().eq("name", role.getName())) > 0) {
+            return ResponseFormat.fail("用户名已存在");
+        }
+        return ResponseFormat.successful(service.saveOrUpdate(role));
+    }
+
+    @PostMapping("/setRole")
+    public ResponseFormat setRole(@RequestBody UserRole userRole) {
+        if (userRole.getUserId() == 1) {
+            return ResponseFormat.fail("不能修改管理员角色");
+        }
+        QueryWrapper<UserRole> userRoleQueryWrapper = new QueryWrapper<>();
+        userRoleQueryWrapper.eq("user_id", userRole.getUserId());
+        Long existUser = userRoleMapper.selectCount(userRoleQueryWrapper);
+        if (existUser == 0) {
+//            添加
+            userRoleMapper.insert(userRole);
+            return ResponseFormat.operate(200, "添加成功", null);
+        } else if (existUser == 1) {
+//            更新
+            userRoleMapper.update(userRole, userRoleQueryWrapper);
+            return ResponseFormat.operate(200, "修改成功", null);
+        }
+        return ResponseFormat.fail("操作失败");
+    }
 
 }
