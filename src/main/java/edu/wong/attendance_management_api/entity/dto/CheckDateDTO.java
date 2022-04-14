9d49e2bd3c5edfc309679dcf0ae1cc3f711a0530
WongSilver <783244553@qq.com>
2022-04-14 08:25:17 +0800

    请假事件和请假人数。用于首页显示

--- src/main/java/edu/wong/attendance_management_api/entity/dto/CheckDateDTO.java
+++ src/main/java/edu/wong/attendance_management_api/entity/dto/CheckDateDTO.java
@@ -1,8 +1,14 @@
 package edu.wong.attendance_management_api.entity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class CheckDateDTO {
     private String byDate;
     private int countNum;
