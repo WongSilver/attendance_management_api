f0e2d24d44b253e1a4774044f44f9a294c1183a1
WongSilver <783244553@qq.com>
2022-04-14 08:25:58 +0800

    style: 优化代码风格

--- src/main/java/edu/wong/attendance_management_api/entity/dto/LoginDTO.java
+++ src/main/java/edu/wong/attendance_management_api/entity/dto/LoginDTO.java
@@ -1,12 +1,18 @@
 package edu.wong.attendance_management_api.entity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
-public class LoginDto implements Serializable {
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO implements Serializable {
 
     //    用户名
     @NotBlank(message = "用户名不能为空")
