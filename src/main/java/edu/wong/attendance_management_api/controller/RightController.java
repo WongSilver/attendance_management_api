235d4aa88e23bbcd161b88bc1418209b8ecbabcd
WongSilver <783244553@qq.com>
2022-04-14 08:27:17 +0800

    新增：list、add、all

--- src/main/java/edu/wong/attendance_management_api/controller/RightController.java
+++ src/main/java/edu/wong/attendance_management_api/controller/RightController.java
@@ -1,18 +1,51 @@
 package edu.wong.attendance_management_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import edu.wong.attendance_management_api.common.lang.ResponseFormat;
+import edu.wong.attendance_management_api.entity.Right;
+import edu.wong.attendance_management_api.service.IRightService;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author WongSilver
  * @since 2022-03-20
  */
-@Controller
+@RestController
 @RequestMapping("/right")
 public class RightController {
+    @Resource
+    IRightService service;
 
+    @GetMapping("/list")
+    public ResponseFormat findAll(@RequestParam(defaultValue = "1") Integer currentPage, @RequestParam(defaultValue = "10") Integer pageSize, @RequestParam(defaultValue = "") String rightName) {
+        IPage<Right> page = new Page<>(currentPage, pageSize);
+        LambdaQueryWrapper<Right> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+
+        lambdaQueryWrapper.like(Strings.isNotEmpty(rightName), Right::getName, rightName);
+
+        IPage<Right> pageData = service.page(page, lambdaQueryWrapper);
+
+        return ResponseFormat.successful(pageData);
+    }
+
+    @PostMapping("/add")
+    public ResponseFormat add(@RequestBody Right right) {
+        service.saveOrUpdate(right);
+        return ResponseFormat.successful(null);
+    }
+
+
+    @GetMapping("/all")
+    public ResponseFormat all() {
+        return ResponseFormat.successful(service.list());
+    }
 }
