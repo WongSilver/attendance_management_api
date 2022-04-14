410d668f2cf178b56c48eff030465e1119537adf
WongSilver <783244553@qq.com>
2022-04-14 08:28:51 +0800

    style: 优化代码风格

--- src/main/java/edu/wong/attendance_management_api/entity/SelectedCourse.java
+++ src/main/java/edu/wong/attendance_management_api/entity/SelectedCourse.java
@@ -3,16 +3,25 @@ package edu.wong.attendance_management_api.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.io.Serializable;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author WongSilver
  * @since 2022-03-20
  */
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @TableName("t_selected_course")
 public class SelectedCourse implements Serializable {
 
@@ -34,34 +43,4 @@ public class SelectedCourse implements Serializable {
      */
     private Integer courseId;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-    public Integer getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(Integer courseId) {
-        this.courseId = courseId;
-    }
-
-    @Override
-    public String toString() {
-        return "SelectedCourse{" +
-            "id=" + id +
-            ", userId=" + userId +
-            ", courseId=" + courseId +
-        "}";
-    }
 }
