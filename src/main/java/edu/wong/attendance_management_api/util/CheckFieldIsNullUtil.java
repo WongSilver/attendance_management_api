0c24bf835d0f504baa733f8b269d22e7d7c3dbdb
WongSilver <783244553@qq.com>
2022-04-18 08:46:29 +0800

    feat: 传入对象和属性名，利用反射，判断传入的属性中是否有空值

---
+++ src/main/java/edu/wong/attendance_management_api/util/CheckFieldIsNullUtil.java
@@ -0,0 +1,23 @@
+package edu.wong.attendance_management_api.util;
+
+import java.lang.reflect.Field;
+
+/**
+ * 判断传入对象的指定属性值是否为空
+ */
+public class CheckFieldIsNullUtil {
+    //    利用反射，取出属性名
+    public static boolean isNull(Object obj, String[] fieldNames) throws IllegalAccessException {
+
+        for (Field f : obj.getClass().getDeclaredFields()) {
+            f.setAccessible(true);
+            for (String fieldName : fieldNames) {
+                if (f.getName().equals(fieldName) && f.get(obj) == null) {
+//                    System.out.println(f.getName() + "---------" + f.get(obj));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
