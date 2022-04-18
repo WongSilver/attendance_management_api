949a65500680e301a3c70e294d71b28942e7e026
WongSilver <783244553@qq.com>
2022-04-18 08:48:28 +0800

    refactor: 把原本ctl中的字段，在service中实现

--- src/main/java/edu/wong/attendance_management_api/service/IUserService.java
+++ src/main/java/edu/wong/attendance_management_api/service/IUserService.java
@@ -1,7 +1,8 @@
 package edu.wong.attendance_management_api.service;
 
-import edu.wong.attendance_management_api.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import edu.wong.attendance_management_api.entity.User;
+import edu.wong.attendance_management_api.entity.dto.UserDTO;
 
 /**
  * <p>
@@ -13,4 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IUserService extends IService<User> {
 
+    UserDTO getUserInfo(Integer id);
 }
--- src/main/java/edu/wong/attendance_management_api/service/impl/UserServiceImpl.java
+++ src/main/java/edu/wong/attendance_management_api/service/impl/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package edu.wong.attendance_management_api.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import edu.wong.attendance_management_api.entity.Account;
+import edu.wong.attendance_management_api.entity.Right;
 import edu.wong.attendance_management_api.entity.User;
+import edu.wong.attendance_management_api.entity.dto.UserDTO;
 import edu.wong.attendance_management_api.mapper.UserMapper;
 import edu.wong.attendance_management_api.service.IUserService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +25,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Resource
+    UserMapper mapper;
+
+    @Override
+    public UserDTO getUserInfo(Integer id) {
+        UserDTO userDTO = new UserDTO();
+
+//        把查到的结果包装成UserDTO
+        List<Account> temp = mapper.getUserDTO(id);
+//        temp.get(0)
+        ArrayList<Right> list = new ArrayList<>();
+        BeanUtil.copyProperties(temp.get(0), userDTO, "userUrl", "userStatus", "userLastTime", "rolId", "rightId", "rightPid", "rightIcon", "rightPagePath");
+        for (Account account : temp) {
+            Right right = new Right();
+            right.setUrl(account.getRightUrl());
+            right.setName(account.getRightName());
+            right.setId(account.getRightId());
+            right.setPid(account.getRightPid());
+            right.setIcon(account.getRightIcon());
+            right.setPagePath(account.getRightPagePath());
+            list.add(right);
+        }
+        userDTO.setRights(list);
+
+        return userDTO;
+    }
 }
